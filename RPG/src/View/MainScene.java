3e599ddb29b9760293c43a23bfa75a661761fe1d
NLYanick <yanick.plaggenmarsch@gmail.com>
2024-12-08 18:09:49 +0100

    Test

--- RPG/src/View/MainScene.java
+++ RPG/src/View/MainScene.java
@@ -1,7 +1,17 @@
 package View;
 
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 
 public class MainScene extends Scene {
 
@@ -11,6 +21,7 @@ public class MainScene extends Scene {
 	public MainScene() {
 		super(new Pane());
 		setUpRoot();
+		setOnKeyTyped(e -> handleInput(e));
 	}
 	
 	private void setUpRoot() {
@@ -18,11 +29,38 @@ public class MainScene extends Scene {
 		
 		root.setPrefSize(SCENEWIDTH, SCENEHEIGHT);
 		
+		// TODO remove
+		BorderPane pane = new BorderPane();
+		Image img = new Image("Images/AngryPear.png");
+		ImageView imgView = new ImageView(img);
+		imgView.setFitWidth(64);
+		imgView.setFitHeight(64);
+		pane.setCenter(imgView);
+		pane.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, null, new BorderWidths(3))));
+		
+		root.getChildren().add(pane);
+		//
+		
 		setRoot(root);
 	}
 	
 	public void setAsRoot(Pane root) {
 		setRoot(root);
+	}
+	
+	private void handleInput(KeyEvent e) {
+		if(e.getCode().equals(KeyCode.UP)) {
+			
+		}
+		if(e.getCode().equals(KeyCode.DOWN)) {
+			
+		}
+		if(e.getCode().equals(KeyCode.RIGHT)) {
+			
+		}
+		if(e.getCode().equals(KeyCode.LEFT)) {
+			
+		}
 	}
 
 }
