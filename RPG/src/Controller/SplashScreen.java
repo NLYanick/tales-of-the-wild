f928d354c332328b578ff83d37f797e5a6efab7c
NLYanick <yanick.plaggenmarsch@gmail.com>
2024-12-22 14:01:36 +0100

    Loading gif

--- RPG/src/Controller/SplashScreen.java
+++ RPG/src/Controller/SplashScreen.java
@@ -4,16 +4,17 @@ import View.MainScene;
 import javafx.application.Preloader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class SplashScreen extends Preloader {
 
-    private final Pane parent = new Pane();
+    private final BorderPane parent = new BorderPane();
 
     private Stage preloaderStage;
 
@@ -21,8 +22,11 @@ public class SplashScreen extends Preloader {
     public void init() throws Exception {
 
         Image image = new Image("Images/Background/Grass.png");
-
         parent.setBackground(new Background(new BackgroundImage(image, null, null, null, null)));
+        
+        Image loadingImage = new Image("Images/Loading.gif");
+        parent.setCenter(new ImageView(loadingImage));
+        
     }
 
     @Override
